1352cc3f49ead37c5702c9e809986aa1edb85f20
TusharBalar <balar.tushar1@gmail.com>
2017-01-20 17:32:39 +0530

    Close complaint done successfully

--- src/customComponent/list/listview.closebtn.component.ts
+++ src/customComponent/list/listview.closebtn.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
-import { Events } from 'ionic-angular';
+import { Events, ModalController, AlertController, ActionSheetController } from 'ionic-angular';
+
+// import service
+import { CustomService } from '../../service/customService';
+import { ComplaintSuggestion } from '../../service/cs.service';
 
 @Component({
   selector: 'nl-close-button',
@@ -18,11 +22,64 @@ export class ListViewCloseButton {
 
   @Input() complaint;
   @Input('master') masterName: string;
+  @Output() closeCmpl = new EventEmitter();
 
-  constructor(public events: Events) { }
+  constructor(public modalCtrl: ModalController,
+              public nl: CustomService,
+              public c: ComplaintSuggestion,
+              public actionSheetCtrl: ActionSheetController,
+              public alertCtrl: AlertController) { }
 
   openCloseModal(complaint) {
-    console.log("DSADASD", this.masterName);
-    this.events.publish(this.masterName + ':close', complaint);
+    let prompt = this.alertCtrl.create({
+      title: 'Do you really want to close ?',
+      message: "",
+      inputs: [{
+        name: 'comment',
+        placeholder: 'Write short description'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        handler: data => {}
+      }, {
+        text: 'Save',
+        handler: data => {
+          this.closeActionSheet(complaint, data);
+        }
+      }]
+    });
+    prompt.present();
   }
+
+  closeActionSheet(complaint, closeComplaintReason) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Are you sure you want to submit ?',
+      buttons: [{
+        text: 'Submit',
+        icon: 'ios-paper-outline',
+        handler: () => {
+          this.nl.showLoader();
+          this.c.closeComplaint(complaint.id, closeComplaintReason).subscribe((res) => {
+            if (res) {
+              this.nl.hideLoader();
+              let data = res.json();
+              this.complaint.statusName = data.statusName;
+              this.complaint.statusId = data.statusId;
+              this.complaint.statusColor = data.statusColor;
+            }
+          }, (err) => {
+            this.nl.hideLoader();
+            this.nl.errMessage();
+          });
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'md-close',
+        role: 'cancel',
+        handler: () => {}
+      }]
+    });
+    actionSheet.present();
+  }
+
 }

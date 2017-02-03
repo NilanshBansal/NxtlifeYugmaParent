a8edb22142a63f5ef07b155464054a513e334f12
AlkaGirdhar <alkagirdhar@AlkaGirdhars-MacBook-Pro.local>
2017-02-03 13:40:22 +0530

    Changed background

--- src/customComponent/navbar.component.ts
+++ src/customComponent/navbar.component.ts
@@ -4,6 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'nl-navbar',
   template: `
     <ion-navbar color="primary">
+    <ion-toolbar color="primary">
       <button ion-button menuToggle>
         <ion-icon name="menu"></ion-icon>
       </button>
@@ -13,6 +14,7 @@ import { Component, Input } from '@angular/core';
       <ion-buttons end>
         <ng-content></ng-content>
       </ion-buttons>
+      </ion-toolbar>
     </ion-navbar>
   `,
   styles: [`

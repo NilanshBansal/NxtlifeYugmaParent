44070196eb20679cd39ade828d10053aa6b3aa5b
TusharBalar <balar.tushar1@gmail.com>
2017-03-01 20:22:43 +0530

    Homework complete

--- src/pages/homework/passed/homework.ts
+++ src/pages/homework/passed/homework.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HomeworkService } from '../../../service/homework.service';
 import { CustomService } from '../../../service/custom.service';
+import { ParentInfo } from '../../../service/parentInfo';
 
 @Component({
   selector: 'passed-homework',
@@ -10,8 +11,82 @@ import { CustomService } from '../../../service/custom.service';
 export class PassedHomework {
 
   public title : string = 'Homework';
+  public homework = [];
+  public students;
+  public child;
+  public currentPage = 1;
+  public standardId;
+  public NoHomework = false;
 
-  constructor() {
+  constructor(private hw : HomeworkService,
+              public parentInfo: ParentInfo,
+              public nl: CustomService) {
+
   }
 
+  ngOnInit() {
+    this.students = this.parentInfo.getStudents();
+    this.standardId = this.parentInfo.getStudents()[0].standardId;
+    this.child = this.students[0];
+    this.getHomework();
+  }
+
+  selectChild(student) {
+    this.standardId = student.standardId;
+    this.getHomework();
+  }
+
+  getHomework() {
+    this.nl.showLoader();
+    this.hw.getOldHomeworkByStandard(this.standardId, this.currentPage).subscribe((res) => {
+      this.onSuccess(res);
+    }, (err) => {
+      this.nl.onError(err);
+    });
+  }
+
+  onSuccess(res) {
+    if (res.status === 204) {
+      this.showEmptyMsg(true);
+    } else {
+      this.showEmptyMsg(false);
+      this.homework = res;
+    }
+    this.nl.hideLoader();
+  }
+
+  doInfinite(infiniteScroll) {
+    this.currentPage += 1;
+    setTimeout(() => {
+      this.loadMoreData(infiniteScroll);
+    }, 500);
+  }
+
+  loadMoreData(infiniteScroll) {
+    this.hw.getHomeworkByStandard(this.standardId, this.currentPage).subscribe((res) => {
+      infiniteScroll.complete();
+      this.loadDataSuccess(res);
+    }, (err) => {
+      infiniteScroll.complete();
+      this.loadDataError(err);
+    });
+  }
+
+  loadDataSuccess(res) {
+    if (res.status === 204) {
+      this.currentPage -= 1;
+      return;
+    }
+    this.homework = this.homework.concat(res);
+  }
+
+  loadDataError(err) {
+    this.currentPage -= 1;
+    this.showEmptyMsg(false);
+    this.nl.onError(err);
+  }
+
+  showEmptyMsg(val) {
+    this.NoHomework = val;
+  }
 }

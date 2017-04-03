e55441d31f6acc4a9a7b25a16df4bfa669a133ed
TusharBalar <balar.tushar1@gmail.com>
2017-04-03 08:58:10 +0530

    Url updated

--- src/service/app.constants.ts
+++ src/service/app.constants.ts
@@ -7,12 +7,14 @@ import { CustomHttpService } from './default.header.service';
 @Injectable()
 export class Configuration {
 
+  public headers;
+  public options;
+  public baseUrl: string = "https://yugma-testing.appspot.com";
+  public Server: string = "https://yugma-testing.appspot.com";
+
   constructor(public http: CustomHttpService,
               public cs: CustomService) {
   }
-
-  headers;
-  options;
 
   getHeader() {
     this.headers = new Headers({
@@ -37,39 +39,37 @@ export class Configuration {
     }
   }
 
-  public Server: string = "https://yugma-testing.appspot.com";
-
   public ComplaintUrl(): string {
     return this.Server + "/parent/" + this.getParentId() + "/";
   }
 
   setUrl(url) {
     this.cs.setHeaderText(url);
-    this.Server = "https://yugma-testing.appspot.com/parent/" + this.getParentId() + "/" + url;
+    this.Server = this.baseUrl + "/parent/" + this.getParentId() + "/" + url;
 
   }
 
   setUrlForStudentSuggestions(url) {
-    this.Server = "https://yugma-testing.appspot.com/parent/" + this.getParentId() + "/suggestion/" + url;
+    this.Server = this.baseUrl + "/parent/" + this.getParentId() + "/suggestion/" + url;
   }
 
   setUrlForSuggestion() {
-    this.Server = "https://yugma-testing.appspot.com/parent/" + this.getParentId() + "/suggestion";
+    this.Server = this.baseUrl + "/parent/" + this.getParentId() + "/suggestion";
   }
 
   setUrlForStudentAppreciations(url) {
-    this.Server = "https://yugma-testing.appspot.com/parent/" + this.getParentId() + "/appreciation/" + url;
+    this.Server = this.baseUrl + "/parent/" + this.getParentId() + "/appreciation/" + url;
   }
 
   setUrlForAppreciations() {
-    this.Server = "https://yugma-testing.appspot.com/parent/" + this.getParentId() + "/appreciation";
+    this.Server = this.baseUrl + "/parent/" + this.getParentId() + "/appreciation";
   }
 
   tokenUpdate(tokenId) {
     const notificationToken = {
       notificationToken: tokenId
     }
-    return this.http.put("https://yugma-testing.appspot.com/parent/" + this.getParentId(), notificationToken)
+    return this.http.put(this.baseUrl + "/parent/" + this.getParentId(), notificationToken)
                     .map((res: Response) => { return res; })
                     .catch((error: any) => Observable.throw(error || 'server error'));
   }

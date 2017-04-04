b18f7e189bf2733a615e98492a08ebdb22d817e9
TusharBalar <balar.tushar1@gmail.com>
2017-04-04 15:13:27 +0530

    Create form for new message

--- src/pages/message/new/new.ts
+++ src/pages/message/new/new.ts
@@ -17,6 +17,7 @@ export class NewMessagePage {
   public headerTitle: string = "New Message";
   newMessage: FormGroup;
   public students;
+  public teachers: boolean = false;
   public student;
   public standardId;
   public studentId;
@@ -30,7 +31,7 @@ export class NewMessagePage {
               public nl: CustomService,
               public parentInfo: ParentInfo,
               public actionSheetCtrl: ActionSheetController) {
-                this.loadForm();
+    this.loadForm();
   }
 
   ionViewWillEnter() {
@@ -38,44 +39,53 @@ export class NewMessagePage {
     if (this.students.length === 1) {
       this.child = this.students[0];  // Auto select for one child
     }
-    
   }
 
-  loadForm() {
+  public loadForm() {
     this.newMessage = this.formBuilder.group({
       student: ['', Validators.required],
       category: ['', Validators.required],
       childCategory: ['', Validators.required],
       againstEmployeeId: ['', Validators.required],
       title: ['', [Validators.required, Validators.maxLength(50)]],
-      description: ['', [Validators.required, Validators.maxLength(200)]],
-      anonymous: [false]
+      message: ['', [Validators.required, Validators.maxLength(200)]]
     });
   }
 
-  selectChild(student) {
+  public selectChild(student) {
     if (student) {
       this.studentId = student.id;
       this.standardId = student.standardId;
-      console.log("AAAA", this.standardId);
-      this.nl.showLoader();
-      this.messageService.getCategories(this.standardId).subscribe((res) => {
-        this.nl.hideLoader();
-        console.log(res);
-        this.categories = res;
-      }, (err) => {
-        console.log("err", err);
-        this.onError(err);
-      })
+      this.getCategories();
     }
+  }
+
+  public getCategories() {
+    this.nl.showLoader();
+    this.messageService.getCategories(this.standardId).subscribe((res) => {
+      this.nl.hideLoader();
+      console.log(res);
+      this.categories = res;
+    }, (err) => {
+      this.onError(err);
+    });
   }
 
   public onError(err) {
     this.nl.onError(err);
   }
 
-  setCategory(category) {
-    console.log("SASASA", category)
+  public setCategory(category) {
+    console.log("SASASA", category);
+    if (category.id === 1) {
+      this.teachers = true;
+    } else {
+      this.teachers = false;
+    }
+  }
+
+  public onSubmit() {
+    console.log(this.newMessage.value)
   }
 
 }

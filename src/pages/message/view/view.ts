0e31ce96be036443789bb088d3fd47d9bbcaf9fa
TusharBalar <balar.tushar1@gmail.com>
2017-04-17 16:39:34 +0530

    Using sockJs and stockJs messaging auto update done successfully

--- src/pages/message/view/view.ts
+++ src/pages/message/view/view.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { ViewController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams, Content } from 'ionic-angular';
 
 // import service
 import { CustomService } from '../../../service/custom.service';
 import { MessageService } from '../../../service/message.service';
 import { ParentInfo } from '../../../service/parentInfo';
+import { CommonService } from '../../../service/common.service'; 
 
 @Component({
   selector: 'view-message',
@@ -19,34 +20,61 @@ export class ViewMessagePage {
   commentForm: FormGroup;
   notPost = true;
   id;
-  userId;
+
+  @ViewChild(Content) content: Content;
 
   constructor(private navParams: NavParams,
               private nl: CustomService,
+              public commonService: CommonService,
               private messageService: MessageService) {
+    this.initForm();
+    this.getData();
+    this.sockJsConnection();
+  }
+
+  ionViewDidEnter() {
+    this.content.scrollToBottom(300);
+  }
+
+  public initForm() {
     this.commentForm = new FormGroup({
       message: new FormControl('', [Validators.required])
     });
-    this.userId = localStorage.getItem("id");
   }
 
-  ionViewWillEnter() {
+  public getData() { 
     this.messages = this.navParams.get('message');
     this.id = this.navParams.get("id");
   }
 
+  public sockJsConnection() {
+    let stompClient = this.commonService.getSockJs();
+    let url = '/parent/conversation/'+ this.id +'/message';
+    let that = this;
+    stompClient.connect({}, function (frame) {
+       stompClient.subscribe(url, function (greeting) {
+          let message = JSON.parse(greeting.body);
+          if (!message) {
+            return;
+          }
+          that.messages.push(message);
+          this.content.scrollToBottom(300);
+       });
+    });
+  }
+
   public postMessage() {
-    console.log(this.commentForm.value);
     this.notPost = false;
     this.messageService.postMessage(this.id, this.commentForm.value).subscribe((res) => {
       this.notPost = true;
       this.messages.push({
         createdAt: new Date(),
-        employeeName: null,
+        employeeId: null,
         message: this.commentForm.value.message,
         parentId: localStorage.getItem("id")
       });
       this.commentForm.reset();
+      this.content.scrollToBottom(300);
     }, (err) => {
       this.nl.errMessage();
       this.notPost = true;

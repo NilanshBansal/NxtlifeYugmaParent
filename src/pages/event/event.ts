91df19a367f4062bd739268d112a85597685dfed
TusharBalar <balar.tushar1@gmail.com>
2017-04-18 17:23:05 +0530

    Minor changes in event

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -4,7 +4,6 @@ import { EventService } from '../../service/event.service';
 import { CustomService } from '../../service/custom.service';
 import { ViewEvent } from './view/event';
 import { TimelinePage } from './timeline/timeline';
-import * as _ from 'underscore';
 import * as moment from 'moment';
 
 @Component({
@@ -14,7 +13,6 @@ import * as moment from 'moment';
     item-inner{
       border-left: 3px solid green !important;
     }
-    
   `]
 })
 
@@ -22,13 +20,8 @@ export class EventComponent {
 
   public eventSource = [];
   public viewTitle;
-  public isToday: boolean;
   public currentDate;
-  public sel_index;
   public hasEvents;
-  public sel_date;
-  public canCreateNewEvent;
-  public _eventSource;
 
   calendar = {
     mode: 'month',
@@ -44,49 +37,32 @@ export class EventComponent {
     this.viewTitle = title;
   }
 
-  onEventSelected(event) {
-    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
-  }
-
-  today() {
-    this.calendar.currentDate = new Date();
-  }
-
-  timeline;
-
   onTimeSelected(ev) {
     console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
           (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
     this.hasEvents = ev.events !== undefined && ev.events.length !== 0;
     this.currentDate = ev.selectedTime.getFullYear() + "-" + (ev.selectedTime.getMonth() + 1);
+    console.log("AAAAA", this.currentDate)
   }
 
   onCurrentDateChanged(event:Date) {
-    var today = new Date();
-    today.setHours(0, 0, 0, 0);
-    event.setHours(0, 0, 0, 0);
-    this.isToday = today.getTime() === event.getTime();
-    this.sel_date = event;
     this.currentDate = event.getFullYear() + "-" + (event.getMonth() + 1);
-    var current = new Date();
-    current.setHours(0, 0, 0);
-    this.canCreateNewEvent = event < today;
   }
 
   createNewEvent() {
+    console.log("CCCC ", this.currentDate)
     this.getAllEvents(this.currentDate);
   }
 
   onRangeChanged(ev: { startTime: Date, endTime: Date }) {
     var eventMonth = this.currentDate;
-    console.log("Res",ev)
     this.eventSource = [];
+    console.log("BBBBB", this.currentDate)
     this.getAllEvents(eventMonth);
   }
 
   getAllEvents(eventMonth) {
     this.eventService.GetEvents(eventMonth).subscribe((res) => {
-      console.log(res)
       if (res.status == 204) {
         this.eventSource.length = 1;
       } else {
@@ -114,7 +90,6 @@ export class EventComponent {
       });
     });
     this.eventSource = tmp;
-    this._eventSource = _.clone(this.eventSource);
   }
 
   markDisabled (date:Date) {
@@ -124,8 +99,6 @@ export class EventComponent {
   }
 
   openViewEventModal(eventId, index) {
-    this._eventSource = _.clone(this.eventSource);
-    this.sel_index = index;
     this.nl.showLoader();
     this.eventService.getEvent(eventId).subscribe((res) => {
       this.onSuccess(res, eventId);
@@ -136,51 +109,8 @@ export class EventComponent {
 
   onSuccess(data, eventId) {
     this.nl.hideLoader();
-    let viewModal = this.modalCtrl.create(ViewEvent, {eventId: eventId, event: data, canEdit: this.canCreateNewEvent});
-    viewModal.onDidDismiss((newVal, deletedEventId) => {
-      if (!newVal) { return; }
-      let hasDelete;
-      if (newVal != "" && !deletedEventId) {
-        hasDelete = this.findAndDelete(newVal.id);
-        this.editEvent(newVal);
-      } 
-      if (deletedEventId) {
-        hasDelete = this.findAndDelete(deletedEventId);
-        this.rebuildArray();
-      }
-    });
+    let viewModal = this.modalCtrl.create(ViewEvent, {eventId: eventId, event: data});
     viewModal.present();
-  }
-
-  findAndDelete(_eventId) {
-    let flag = false;
-    this._eventSource.forEach((val, index) => {
-      if (val.id === _eventId) {
-        this._eventSource.splice(index, 1);
-        flag = true;
-        return flag;
-      }
-    });
-  }
-
-  rebuildArray() {
-    this.eventSource = this._eventSource;
-  }
-
-  editEvent(newVal) {
-    this._eventSource.push({
-      id: newVal.id,
-      startTime: moment(newVal.start).toDate(),
-      endTime: moment(newVal.end).toDate(),
-      title: newVal.title,
-      allDay: false,
-      location: newVal.location,
-      time1: newVal.startTime,
-      time2: newVal.endTime,
-      color: newVal.plannerTypeColor,
-      durationDays: newVal.durationDays
-    });
-    this.rebuildArray();
   }
 
   onError(err) {
@@ -188,12 +118,7 @@ export class EventComponent {
   }
 
   openTimelineModal() {
-    this._eventSource = _.clone(this.eventSource);
     let timelineModal = this.modalCtrl.create(TimelinePage);
-    timelineModal.onDidDismiss((newVal, deletedEventId) => {
-      this.eventSource = [];
-      this.getAllEvents(this.currentDate);
-    });
     timelineModal.present();
   }
 

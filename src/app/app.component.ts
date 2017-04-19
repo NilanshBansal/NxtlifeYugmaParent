a9c6a903b272de16afe570513d85a3860499ca2e
akash-gupta-146 <ag.akash007@gmail.com>
2017-04-19 12:19:39 +0530

    Added colored icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,20 +61,20 @@ export class MyApp extends UserSessionManage {
 
   sidebarConfig() {
     this.pages = [
-      { title: 'Home', component: Dashboard, icon: 'ios-home-outline', url: 'dashboard' },
-      { title: 'Complaints', component: ComplaintPage, icon: 'ios-sad-outline', url: 'complaint' },
-      { title: 'Suggestions', component: SuggestionTabs, icon: 'ios-bulb-outline', url: 'suggestion' },
-      { title: 'Appreciations', component: AppreciationTabs, icon: 'ios-thumbs-up-outline', url: 'appreciation' },
-      { title: 'Messaging', component: MessagePage, icon: 'ios-chatbubbles-outline', url: 'conversation' },
-      { title: 'Calendar',component: PlannerComponent , icon: 'ios-calendar-outline', url: 'planner'},
-      { title: 'Poll', component: PollPage, icon: 'ios-stats-outline', url: 'poll' },
-      { title: 'Survey', component: SurveyListPage, icon: 'ios-analytics-outline', url: 'survey' },
-      { title: 'Homework' , component : HomeworkTabs , icon : 'ios-book-outline' , url : 'homework' },
-      { title: 'Circular',component : CircularComponent , icon : 'ios-paper-outline' , url : 'circular' },
-      { title: 'Student Rating', component: StudentRating, icon: 'ios-pulse-outline', url: 'student-profile' },
+      { title: 'Home', component: Dashboard, icon: 'http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-4/256/home-icon.png', url: 'dashboard' },
+      { title: 'Complaints', component: ComplaintPage, icon: 'http://www.clipartkid.com/images/108/beschreibung-smirc-thumbsdown-svg-XD7fzL-clipart.png', url: 'complaint' },
+      { title: 'Suggestions', component: SuggestionTabs, icon: 'https://www.thecaribbeancurrent.com/wp-content/uploads/2014/11/A-good-idea.png', url: 'suggestion' },
+      { title: 'Appreciations', component: AppreciationTabs, icon: 'http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c4c4.png', url: 'appreciation' },
+      { title: 'Messaging', component: MessagePage, icon: 'https://lh3.ggpht.com/6qvGDjN7jg9G6cuZ0szgLS4U-CNm0JoK1a21J-inFltDogLdIycvVDo2L7_Zjzcw9A=w300', url: 'conversation' },
+      { title: 'Calendar',component: PlannerComponent , icon: 'http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-7/256/Calendar-icon.png', url: 'planner'},
+      { title: 'Poll', component: PollPage, icon: 'https://cdn2.iconfinder.com/data/icons/Siena/256/poll%20green.png', url: 'poll' },
+      { title: 'Survey', component: SurveyListPage, icon: 'http://www.freeiconspng.com/uploads/survey-icon-12.png', url: 'survey' },
+      { title: 'Homework' , component : HomeworkTabs , icon : 'http://a5.mzstatic.com/us/r30/Purple1/v4/a6/9f/54/a69f54ff-d7c7-bf53-e3d2-75e9bf704b11/icon256.png' , url : 'homework' },
+      { title: 'Circular',component : CircularComponent , icon : 'https://www.designswan.com/wp-content/uploads/2009/icon/3dOffice/3dicon21.png' , url : 'circular' },
+      { title: 'Student Rating', component: StudentRating, icon: 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/256/Actions-rating-icon.png', url: 'student-profile' },
     ];
     this.account = [
-      { title: 'Account', component: AccountPage, icon: 'ios-contact-outline' }
+      { title: 'Account', component: AccountPage, icon: 'https://cdn4.iconfinder.com/data/icons/free-large-boss-icon-set/512/Caucasian_boss.png' }
     ];
   }
 

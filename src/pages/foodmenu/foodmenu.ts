1b3be4f14547c3f1d2024a615f21514fd3062a34
TusharBalar <balar.tushar1@gmail.com>
2017-04-19 11:04:32 +0530

    Get foodmenu data from server

--- src/pages/foodmenu/foodmenu.ts
+++ src/pages/foodmenu/foodmenu.ts
@@ -1,14 +1,73 @@
-import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular/index';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { EventService } from '../../service/event.service';
 
 @Component({
-  selector: 'foodmenu',
-  templateUrl: 'foodmenu.html'
+  templateUrl: "foodmenu.html"
 })
 
 export class FoodMenu {
 
-  public title: string = "Foodmenu";
+  eventSource;
+  viewTitle;
+  title: string = "FoodMenu";
 
-  constructor() { }
+  isToday: boolean;
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  };
 
+  constructor(private navController: NavController,
+              private foodmenu: EventService) {
+  }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  onEventSelected(event) {
+    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+  }
+
+  changeMode(mode) {
+    this.calendar.mode = mode;
+  }
+
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  onTimeSelected(ev) {
+    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+  }
+
+  onCurrentDateChanged(event: Date) {
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    event.setHours(0, 0, 0, 0);
+    this.isToday = today.getTime() === event.getTime();
+    console.log("AAAAA", this.today);
+    this.getFoodMenu();
+  }
+
+  onRangeChanged(ev) {
+    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+  }
+
+  getFoodMenu() {
+    console.log("AAAA");
+    this.foodmenu.getFoodMenu().subscribe((res) => {
+      console.log("RES", res);
+    }, (err) => {
+      console.log("err", err)
+    })
+  }
+
+  markDisabled = (date: Date) => {
+    var current = new Date();
+    current.setHours(0, 0, 0);
+    return date < current;
+  };
 }

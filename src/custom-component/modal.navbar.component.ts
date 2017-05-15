66d3c63d00fc139c14f773f8d11e3e953cc7add2
TusharBalar <balar.tushar1@gmail.com>
2017-05-15 15:48:42 +0530

    Custom header update

--- src/custom-component/modal.navbar.component.ts
+++ src/custom-component/modal.navbar.component.ts
@@ -6,15 +6,18 @@ import { Events, ViewController } from 'ionic-angular';
   template: `
      <ion-navbar color="primary">
       <ion-toolbar>
-          <ion-buttons start>
-            <button ion-button color="light" class="navBtnRight" (click)="dismiss()">
-              <span color="light" showWhen="ios">Cancel</span>
-              <ion-icon color="light" name="md-close" showWhen="android,windows" class="csBigIcon"></ion-icon>
-            </button>
-          </ion-buttons>
-          <ion-title>
-            {{title | uppercase}}
-          </ion-title>
+        <ion-buttons start>
+          <button ion-button color="light" class="navBtnRight" (click)="dismiss()">
+            <span color="light" showWhen="ios">Cancel</span>
+            <ion-icon color="light" name="md-close" showWhen="android,windows" class="csBigIcon"></ion-icon>
+          </button>
+        </ion-buttons>
+        <ion-title>
+          {{title | uppercase}}
+        </ion-title>
+        <ion-buttons end>
+          <ng-content></ng-content>
+        </ion-buttons>
       </ion-toolbar>   
     </ion-navbar>
   `,

958985a832a3b335a070795d3e3a51e5bd7bcc19
TusharBalar <balar.tushar1@gmail.com>
2017-05-31 12:44:14 +0530

    Fix issue with no survey

--- src/pages/survey/list/survey-list.ts
+++ src/pages/survey/list/survey-list.ts
@@ -1,119 +1,128 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SurveyService } from '../../../service/survey.service';
-import { NavController,ModalController,Events } from 'ionic-angular';
+import { NavController, ModalController, Events } from 'ionic-angular';
 import { ViewSurvey } from '../view/view';
 import { SurveyPage } from '../survey';
 import { CustomService } from '../../../service/custom.service';
 
 @Component({
-    selector : 'survey-list',
-    templateUrl : 'survey-list.html'
+    selector: 'survey-list',
+    templateUrl: 'survey-list.html'
 })
 
-export class SurveyListPage implements OnInit{
+export class SurveyListPage implements OnInit {
 
     public title: string = "Survey";
     public allsurveys;
     public onesurveys;
     public allData = [];
-    public EmptySurveys : boolean = false;
+    public EmptySurveys: boolean = false;
     public currentPage = 1;
     public indexxx;
 
-    constructor(private _surveyServ : SurveyService ,
-                private navCtrl : NavController,
-                private nl : CustomService ,
-                private _event : Events , 
-                private modalCtrl : ModalController){
-         this.getSurveys();
-         this._event.subscribe('survey:done',
-                     (data) => {console.log("DSFFDF", data); this.surveyDoneSplice(data);})
+    constructor(private _surveyServ: SurveyService,
+        private navCtrl: NavController,
+        private nl: CustomService,
+        private _event: Events,
+        private modalCtrl: ModalController) {
+        this.getSurveys();
+        this._event.subscribe('survey:done',
+            (data) => { console.log("DSFFDF", data); this.surveyDoneSplice(data); })
 
-         
+
     }
 
-    surveyDoneSplice(data){
-        this.allsurveys.splice(data,1);
+    surveyDoneSplice(data) {
+        this.allsurveys.splice(data, 1);
     }
 
-    getSurveys(){
+    getSurveys() {
         this.nl.showLoader();
         this._surveyServ.getallsurveys(1)
-            .subscribe( data => { this.allsurveys = data ; this.nl.hideLoader();  console.log('surveys',this.allsurveys);},
-                () => console.log('allsurveys',this.allsurveys))
+            .subscribe(data => {
+                this.nl.hideLoader();
+                 if (data.status == 204) {
+                    this.EmptySurvey = true;
+                } else {
+                    this.allsurveys = data;  console.log('surveys', this.allsurveys); }},
+            () => console.log('allsurveys', this.allsurveys))
     }
 
-    getParticularSurvey(surveyId,indexx){
-        console.log('indexx',indexx);
+    EmptySurvey: boolean = false;
+
+    getParticularSurvey(surveyId, indexx) {
+        console.log('indexx', indexx);
         this.indexxx = indexx;
         this.nl.showLoader();
         this._surveyServ.getOneSurvey(surveyId)
-        .subscribe( data => { this.onesurveys = data ;this.nl.hideLoader(); this.clickablesurvey(this.onesurveys,indexx)},
-              () => console.log('onesurveys',this.onesurveys))
+            .subscribe(data => {
+                this.onesurveys = data; this.nl.hideLoader(); this.clickablesurvey(this.onesurveys, indexx)
+            },
+            () => console.log('onesurveys', this.onesurveys));
     }
 
-   clickablesurvey(objj,indexx){
-    //    console.log('clickablesurvey');
-    //    this.navCtrl.push(SurveyPage,{
-    //        objj : objj
-    //    });
+    clickablesurvey(objj, indexx) {
+        //    console.log('clickablesurvey');
+        //    this.navCtrl.push(SurveyPage,{
+        //        objj : objj
+        //    });
 
-       let modal4 = this.modalCtrl.create(ViewSurvey,{ objj : objj , indexx : indexx});
-       modal4.present();
-   }
+        let modal4 = this.modalCtrl.create(ViewSurvey, { objj: objj, indexx: indexx });
+        modal4.present();
+    }
 
 
 
- doRefresh(refresher) {
-    setTimeout(() => {
-         this._surveyServ.getallsurveys(1).subscribe((res) => {
+    doRefresh(refresher) {
+        setTimeout(() => {
+            this._surveyServ.getallsurveys(1).subscribe((res) => {
                 this.onSuccess(res);
                 refresher.complete();
             }, (err) => {
                 refresher.complete();
-                 this.onError(err);
+                this.onError(err);
             });
         }, 500);
-     }
+    }
 
     onSuccess(res) {
         this.nl.hideLoader();
-            if (res.status === 204) {
-             this.EmptySurveys = true;
-            }
-            else{
-                this.EmptySurveys = false;
-                this.allsurveys = res;
-           }
-     }
+        if (res.status === 204) {
+            this.EmptySurveys = true;
+        }
+        else {
+            this.EmptySurveys = false;
+            this.allsurveys = res;
+        }
+    }
 
     onError(err) {
-       this.nl.onError(err);
+        this.nl.onError(err);
     }
 
 
-     doInfinite(infiniteScroll) {
-         this.currentPage += 1;
-          setTimeout(() => {
+    doInfinite(infiniteScroll) {
+        this.currentPage += 1;
+        setTimeout(() => {
             this._surveyServ.getallsurveys(this.currentPage).subscribe(response => {
-              if (response.status === 204) {
+                if (response.status === 204) {
+                    this.currentPage -= 1;
+                    infiniteScroll.complete();
+                    return;
+                }
+                console.log('response', response);
+                this.allsurveys = this.allsurveys.concat(response);
+                // this.pop();
+                infiniteScroll.complete();
+            }, (err) => {
                 this.currentPage -= 1;
                 infiniteScroll.complete();
-                return;
-              }
-              console.log('response',response);
-              this.allsurveys = this.allsurveys.concat(response);
-             // this.pop();
-              infiniteScroll.complete();
-            }, (err) => {
-              this.currentPage -= 1;
-              infiniteScroll.complete();
             });
-          }, 1000);
+        }, 1000);
     }
 
 
-    ngOnInit():void{
+    ngOnInit(): void {
 
     }
 }
